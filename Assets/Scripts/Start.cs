1111a90be061812e7d8295fe234d3703f58742d9
Alexis Haldy <alexishaldy@gmail.com>
2022-12-14 10:51:03 +0100

    Finish car test

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add a car controller to the scene
+        var car = new CarController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
